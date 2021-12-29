052d8aea8d8401d2d9394de70ee2b3c80160b19f
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-29 01:26:33 -0300

    feat: add typing to data from the controller layer

--- register-developer-backend/src/developer/services/get-all-developer.service.ts
+++ register-developer-backend/src/developer/services/get-all-developer.service.ts
@@ -3,8 +3,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Like, Repository } from 'typeorm';
 import { Developer } from '../domain/developer.entity';
 import { IGetAllDeveloperService } from '../interfaces/services/get-all-developer.service.interface';
-import { paginateResponse } from '../../common/paginate-response';
+import { paginateResponse } from '../../common/functions/paginate-response';
 import { DeveloperPaginationDomain } from '../domain/developer-pagination.domain';
+import { PartialDeveloperPaginationDomain } from '../domain/partial-developer-pagination.domain';
 
 @Injectable()
 export class GetAllDeveloperService implements IGetAllDeveloperService {
@@ -13,12 +14,9 @@ export class GetAllDeveloperService implements IGetAllDeveloperService {
     private developerRepository: Repository<Developer>,
   ) {}
 
-  async getAll(query: {
-    take: number;
-    page: number;
-    skip: number;
-    keyword: string;
-  }): Promise<DeveloperPaginationDomain> {
+  async getAll(
+    query: PartialDeveloperPaginationDomain,
+  ): Promise<DeveloperPaginationDomain> {
     const take = query.take || 10;
     const page = query.page || 1;
     const skip = (page - 1) * take;
