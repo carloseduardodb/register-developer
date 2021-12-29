df2e336bc4e6508a900b733ba54ecbf058234b92
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-29 01:30:09 -0300

    feat: prohibit incremental id return to client

--- register-developer-backend/src/level/domain/level.entity.ts
+++ register-developer-backend/src/level/domain/level.entity.ts
@@ -1,13 +1,29 @@
-import { Entity, Column, PrimaryGeneratedColumn, Generated } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  Generated,
+  OneToMany,
+} from 'typeorm';
+import { Developer } from '../../developer/domain/developer.entity';
 
 @Entity()
 export class Level {
   @PrimaryGeneratedColumn('increment')
   level_id: number;
 
+  @Column()
   @Generated('uuid')
   level_uuid: string;
 
   @Column()
   name: string;
+
+  @OneToMany((type) => Developer, (developer) => developer.level)
+  developers: Developer[];
+
+  toJSON() {
+    delete this.level_id;
+    return this;
+  }
 }
