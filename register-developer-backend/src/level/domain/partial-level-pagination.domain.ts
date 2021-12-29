a921de8f2dbdfdc95da940922b947a6e9e028a9f
carloseduardodb <carloseduardodiasbatista@gmail.com>
2021-12-29 02:48:56 -0300

    feat:  update implements

--- register-developer-backend/src/level/domain/partial-level-pagination.domain.ts
+++ register-developer-backend/src/level/domain/partial-level-pagination.domain.ts
@@ -1,7 +1,9 @@
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class PartialLevelPaginationDomain {
+export class PartialLevelPaginationDomain
+  implements PartialLevelPaginationDomain
+{
   @Type(() => Number)
   @IsNumber()
   @IsOptional()
