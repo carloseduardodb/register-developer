8d28cb8a1ae3fe4e0a197ed063dfc469a1fc8665
carloseduardodb <carloseduardodiasbatista@gmail.com>
2022-01-01 17:05:46 -0300

    feat: configure navigation to return page

--- register-developer-frontend/src/components/HeaderActions/index.tsx
+++ register-developer-frontend/src/components/HeaderActions/index.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { FiArrowLeft } from "react-icons/fi";
+import { useNavigate } from "react-router-dom";
+import { useDeveloper } from "../../hooks/useDeveloper";
 
 const HeaderActions = () => {
+  const navigate = useNavigate();
+  const { setStatusModal, setTitle } = useDeveloper();
+
+  const handleCreateModal = () => {
+    setTitle("Cadastrar Desenvolvedor");
+    setStatusModal(true);
+  };
+
   return (
     <>
       <div className="flex justify-between px-1 py-5">
-        <button className="bg-p-blue transform hover:scale-105 transition-transform delay-100 duration-200 ease-in rounded-full p-2">
+        <button
+          onClick={() => navigate(-1)}
+          className="bg-p-blue hover:bg-p-blue-dark transform hover:scale-105 transition-transform delay-100 duration-200 ease-in rounded-full p-2"
+        >
           <FiArrowLeft size={25} color="white" />
         </button>
-        <button className="bg-p-blue transform hover:scale-105 transition-transform delay-100 duration-200 ease-in rounded-md px-5 text-white">
-          Create
+        <button
+          onClick={handleCreateModal}
+          className="bg-p-blue hover:bg-p-blue-dark transform hover:scale-105 transition-transform delay-100 duration-200 ease-in rounded-md px-5 text-white"
+        >
+          Criar
         </button>
       </div>
     </>
