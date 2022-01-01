19d63408b683d9f6ec322517ccdcff9fb615bd93
carloseduardodb <carloseduardodiasbatista@gmail.com>
2022-01-01 17:02:58 -0300

    fix: sum errors in string type

--- register-developer-backend/src/developer/domain/developer-pagination.domain.ts
+++ register-developer-backend/src/developer/domain/developer-pagination.domain.ts
@@ -2,9 +2,9 @@ import { DeveloperDomain } from './developer.domain';
 export class DeveloperPaginationDomain {
   statusCode: string;
   data: DeveloperDomain[];
-  count: any;
-  currentPage: any;
-  nextPage: any;
+  count: number;
+  currentPage: number;
+  nextPage: number;
   prevPage: number;
   lastPage: number;
 }
