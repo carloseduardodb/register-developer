1a610ee380aec5590a4c9f6cbdc5d83bb072cfa4
carloseduardodb <carloseduardodiasbatista@gmail.com>
2022-01-01 17:02:50 -0300

    fix: sum errors in string type

--- register-developer-backend/src/common/functions/paginate-response.ts
+++ register-developer-backend/src/common/functions/paginate-response.ts
@@ -1,13 +1,13 @@
-export function paginateResponse(data, page, limit = 10) {
+export function paginateResponse(data, page, limit = 5) {
   const [result, total] = data;
   const lastPage = Math.ceil(total / limit);
-  const nextPage = page + 1 > lastPage ? null : page + 1;
-  const prevPage = page - 1 < 1 ? null : page - 1;
+  const nextPage = Number(page) + 1 > lastPage ? null : Number(page) + 1;
+  const prevPage = Number(page) - 1 < 1 ? null : Number(page) - 1;
   return {
     statusCode: 'success',
     data: [...result],
     count: total,
-    currentPage: page,
+    currentPage: Number(page),
     nextPage: nextPage,
     prevPage: prevPage,
     lastPage: lastPage,
