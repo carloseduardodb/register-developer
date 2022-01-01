18f19059abbe3361f0135701ea1300f377fddea1
carloseduardodb <carloseduardodiasbatista@gmail.com>
2022-01-01 17:07:15 -0300

    feat: add edit create modal unique to project

--- register-developer-frontend/src/components/ModalEditCreate/index.tsx
+++ register-developer-frontend/src/components/ModalEditCreate/index.tsx
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-const ModalEditCreate = () => {
-  return <div></div>;
+type modalProps = {
+  children: ReactNode;
+  showModal: boolean;
+  setShowModal: (data: boolean) => void;
+  title: string;
+  idEditable: string;
+};
+
+const ModalEditCreate = ({
+  children,
+  showModal = true,
+  setShowModal,
+  title,
+  idEditable,
+}: modalProps) => {
+  return (
+    <>
+      {showModal ? (
+        <>
+          <div className="justify-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid rounded-t">
+                  <h3 className="text-3xl font-semibold">{title}</h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                {children}
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid rounded-b">
+                  <button
+                    className="text-blue-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Fechar
+                  </button>
+                  <button
+                    className="bg-p-blue text-white active:bg-blue-600 hover:bg-p-blue-dark font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Salvar {idEditable !== "" ?? "Alterações"}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
 };
 
 export default ModalEditCreate;
