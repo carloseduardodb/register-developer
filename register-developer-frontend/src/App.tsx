0aac39733f390fc5016e2217b00f5883d824ec65
carloseduardodb <carloseduardodiasbatista@gmail.com>
2022-01-02 17:37:01 -0300

    feat: updates parent component by adding toast fy main element and its styles

--- register-developer-frontend/src/App.tsx
+++ register-developer-frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { AlertSystemProvider } from "./context/AlertSystemContext";
 import Routes from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <AlertSystemProvider>
         <Routes />
+        <ToastContainer />
       </AlertSystemProvider>
     </>
   );
