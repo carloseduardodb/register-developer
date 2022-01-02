116e4ed12e7c68291a360f4bfba6466f0e9baad9
carloseduardodb <carloseduardodiasbatista@gmail.com>
2022-01-02 17:48:06 -0300

    feat: remove liability from the levels page

--- register-developer-frontend/src/pages/ManageLevels/index.tsx
+++ register-developer-frontend/src/pages/ManageLevels/index.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import HeaderActions from "../../components/HeaderActions/index";
+import { ManagerLevelProvider } from "../../context/ManagerLevelContext";
+import { UpdateTableProvider } from "../../context/UpdateTableContext";
+import ManagerLevelModals from "../../components/ManagerLevelModals/index";
+import ManagerDeveloperTable from "../../components/ManagerDeveloperTable";
+import ManagerLevelTable from "../../components/ManagerLevelTable/index";
 
 const ManageLevels = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="flex justify-center bg-p-blue-light items-center h-screen">
+        <main className="w-full max-w-6xl">
+          <UpdateTableProvider>
+            <ManagerLevelProvider>
+              <HeaderActions />
+              <br />
+
+              <ManagerLevelTable />
+
+              <ManagerLevelModals />
+            </ManagerLevelProvider>
+          </UpdateTableProvider>
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default ManageLevels;
